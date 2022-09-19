1d757384cfef26c7f3b1d92932a9eb2a6e03b2bb
Mohamed Metwalli <m.metwalli@outlook.com>
2022-09-19 06:27:03 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <input type="file" accept=".obj"></input>
+    </div>
+  );
+}
---
+++ src/components/objViewer.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
+import * as THREE from "three";
+import OrbitControlsModule from "three-orbit-controls";
+import { OBJLoader } from "three-addons";
+import { ScaleLoader } from "react-spinners";
+
+const OrbitControls = OrbitControlsModule(THREE);
+
+export default function OBJViewer(props) {
+  return {};
+}
+
+OBJViewer.propTypes = {
+  className: PropTypes.string,
+  url: PropTypes.string,
+  file: PropTypes.object,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  backgroundColor: PropTypes.string,
+  modelColor: PropTypes.string,
+  sceneClassName: PropTypes.string,
+  onSceneRendered: PropTypes.func
+};
